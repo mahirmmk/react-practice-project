50eb9b7b47f3be3266e3fd204ed851b22a6ef0ca
mahir <mahir9908@outlook.com>
2022-11-12 21:01:04 +0530

    minor bugs fixed

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 function Projects() {
   return (
-    <div className=' bg bg-gradient-to-b from-red-100 to-slate-200' >
+    <div className=''>
         <h1 className='text-2xl text-center'>List of react based mini projects</h1>
-        <div className="flex flex-wrap -mb-4 mx-2 mt-10 ">
+        <div className="flex justify-center items-center text-center   h-28 flex-wrap">
             <CategoryBox path="/home" text="Home" />
             <CategoryBox path="/todo" text="To-do App" />
             <CategoryBox path="/counter" text="Counter app" />
@@ -20,11 +20,12 @@ function Projects() {
 const CategoryBox=(props)=>{
 
     return<>
-      <div className="w-full  md:w-1/3 mb-8 px-2  ">
-        <Link  to={props.path} > <div   className="h-32 rounded-3xl shadow-md bg-indigo-200 text-center text-4xl md:text-6xl  hover:bg-blue-500 hover:text-blue-50 transition-all duration-300 "> {props.text} </div></Link>
+      <div  className='md:w-1/4 w-80 m-3 h-full shadow-lg rounded-lg bg-blue-800 hover:bg-blue-500 duration-300 hover:shadow-md ' >
+        <Link to={props.path} >
+         <button className='h-full w-full text-4xl text-white '> {props.text} </button>
+        </Link>
       </div>
-    </>
-  
+    </>  
   }
 
 export default Projects
